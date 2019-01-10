05d12dc6f3b1c8867864ac7c52fe7bd99c04d71e
David Morisset <dmorisset@gmail.com>
2019-01-10 20:23:39 +0000

    added the correct class to blog images added todo file to .gitignore

--- app.js
+++ app.js
@@ -298,7 +298,7 @@ let blogRender = function(callback, options) {
       article +='<p>' + blog.text.replace(/\r?\n/g, '<br />') + '</p>'
 
       if (blog.image) {
-        article += '<img src="blog/' +blog.image + '">'
+        article += '<img class="blog-image" src="blog/' +blog.image + '">'
       }
 
       article += '</article>';
